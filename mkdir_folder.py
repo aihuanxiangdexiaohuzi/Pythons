df17aa841e5bd7f9a16c8457ba8fc63f9ce88eac
aihuanxiangdexiaohuzi <1171588613@qq.com>
2017-07-09 18:32:26 +0800

    add error handing

--- mkdir_folder.py
+++ mkdir_folder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# -*- conding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
-'mkdir_folder for every day '
+'a mkdir_folder'
 
 __author__ = 'Moustache'
 
@@ -13,8 +13,8 @@ basename= str(input('please input your name:'))
 if not basename.strip():
     basename= 'hlt'
 
-basePath = 'G:\\work\\'
-
+basePath = 'G:\\work\\' #folder path
+   
 thisYear = str(time.localtime()[0])
 thisMonth = str(time.localtime()[1])
 thisDay = time.strftime("%Y%m%d", time.localtime())
@@ -27,7 +27,6 @@ monthPath = yearPath + '\\' + thisMonth
 dayPath = monthPath + '\\' + basename+thisDay
 
 def mkdir_folder():
-    
     if not os.path.exists(yearPath):    
         os.mkdir(yearPath)
     if not os.path.exists(monthPath):
@@ -35,12 +34,20 @@ def mkdir_folder():
     if not os.path.exists(dayPath):
         os.mkdir(dayPath)
         
+    #open the folder   
     os.popen("explorer.exe" + " " + dayPath)
     os.popen("exit")
         
-def main():
-    mkdir_folder()
-
+def main():    
+    try:       
+        mkdir_folder()
+    except Exception as e:
+        print('exception:', e)
+        print('Failed to create folder,Please enter the correct folder name!')
+    else:
+        print('Successfully created the folder!!')
+    print('END')
+    
 
 if __name__ == '__main__':
     main()
