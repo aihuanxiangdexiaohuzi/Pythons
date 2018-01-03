cba54d366c8ce3923ab79f4065e5ef85c577879e
hltsir <1171588613@qq.com>
2018-01-03 22:16:03 +0800

    update gui and version 1.0.1

--- mkdir_folder.py
+++ mkdir_folder.py
@@ -2,29 +2,19 @@
 # -*- coding:utf-8 -*-
 
 'a mkdir_folder'
+# version ：1.0.1
 
 __author__ = 'Moustache'
 
-import time,os
+import time,os, sys
 from tkinter import *
 import tkinter.messagebox as messagebox
 
-#basename= str(input('please input your name:'))
-#basename = ' '
-#basePath = ' '
 underline = '_'
 chinese = 'chinese'
 english = 'english'
 
-#default
-#if not basename.strip():
- #   basename= 'hlt'
-
-#basePath = 'G:\\work\\' #folder path
-
-#创建文件函数
-
-
+#类
 class Application(Frame):
     
     def __init__(self, master=None):
@@ -34,15 +24,15 @@ class Application(Frame):
         
     def createWidgets(self):
         #名字输入提示
-        self.nameTip = Label(self, text = "请输入你的名字：", fg = "red", bg = 'yellow').pack()
+        self.nameTip = Label(self, text = "请输入你的名字：", fg = "red", bg = 'yellow', font = ('Arial, 12')).pack()
 
         # 名字输入
-        self.name = StringVar()
+        self.name = StringVar() #定义变量
         self.name.set("hlt")  # 设置默认用户名
         self.nameInput = Entry(self, textvariable=self.name).pack()
 
         #路径输入提示
-        self.pathTip = Label(self, text="请输入你的路径：", fg="red", bg="yellow").pack()
+        self.pathTip = Label(self, text="请输入你的路径：", fg="red", bg="yellow", font = ('Arial, 12')).pack()
 
         # 路径输入
         self.path = StringVar()
@@ -50,15 +40,16 @@ class Application(Frame):
         self.pathInput = Entry(self, textvariable=self.path).pack()
 
         #确认
-        self.confirmButton = Button(self, text='确定', fg = "green", command= self.varGet, bg = "yellow").pack()
+        self.confirmButton = Button(self, text='确定', fg = "green", command= self.mkdirFloder, bg = "yellow", font = ('Arial, 12')).pack()
         
-    #获取输入函数
-    def varGet(self):
+    #创建文件夹
+    def mkdirFloder(self):
         name = self.name.get()
         basename = name
 
         path = self.path.get()
         basePath = path
+
         thisYear = str(time.localtime()[0])
         thisMonth = str(time.localtime()[1])
         thisDay = time.strftime("%Y%m%d", time.localtime())
@@ -71,54 +62,37 @@ class Application(Frame):
         dayPath = monthPath + '\\' + basename + underline + thisDay + underline + chinese
         englishPath = monthPath + '\\' + basename + underline + thisDay + underline + english
 
-        #try:
-        if not os.path.exists(basePath):
-            os.mkdir(basePath)
-        if not os.path.exists(yearPath):
-            os.mkdir(yearPath)
-        if not os.path.exists(monthPath):
-            os.mkdir(monthPath)
-        if not os.path.exists(dayPath):
-            os.mkdir(dayPath)
-        if not os.path.exists(englishPath):
-            os.mkdir(englishPath)
+        try:
+        # 创建文件函数
+            if not os.path.exists(basePath):
+                os.mkdir(basePath)
+            if not os.path.exists(yearPath):
+                os.mkdir(yearPath)
+            if not os.path.exists(monthPath):
+                os.mkdir(monthPath)
+            if not os.path.exists(dayPath):
+                os.mkdir(dayPath)
+            if not os.path.exists(englishPath):
+                os.mkdir(englishPath)
+        except Exception as e:
+            print('exception:', e)
+            print('Failed to create folder,Please enter the correct folder name!')
+        else:
+             messagebox.showinfo('Successfully' , "Successfully created the folder!!")
 
-        #except Exception as e:
-            #print('exception:', e)
-            #print('Failed to create folder,Please enter the correct folder name!')
-        #else:
-            # messagebox.showinfo('Successfully' , "Successfully created the folder!!")
-            # open the folder
-        os.popen("explorer.exe" + " " + dayPath)
+        #open the folder
+        os.popen("explorer.exe" + " " + monthPath)
         os.popen("exit")
 
-        #print('END')
-
-
-
-
-
-    #def mkdir_folder(self):
-
-
-        #self.okLabel = Label(self, text="Successfully ", fg="green")
-        #self.okLabel.pack()
-
-    def Successfully(self):
-        self.okButton = Button(self, text = "Successfully ", fg = "green")
-        self.okButton.pack(side = "bottom")
-
 def main():
-    
     #实例化对象
     app = Application()
     #设置窗口标题
     app.master.title('Mkdir_folder')
+    #设置主窗口大小
+    app.master.geometry("400x120")
     #主消息循环
     app.mainloop()
-
-    
-    
 
 if __name__ == '__main__':
     main()
